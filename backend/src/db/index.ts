c7e7177170034dbf55d5e2237382a408e60dc594
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-10-19 18:41:36 +0530

    feat: user table in mongoDB

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const { Schema, Model } = mongoose;
 
 dotenv.config();
 const mongo_url = process.env.MONGODB_URI;
@@ -7,4 +8,15 @@ const mongo_url = process.env.MONGODB_URI;
 mongoose
   .connect(mongo_url)
   .then(() => console.log("MongoDB connected"))
-  .catch((err: Error) => console.log("Error occurred: ", err));
+  .catch((err: Error) => console.log("MongoDB Connection error: ", err));
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  firstName: String,
+  lastName: String
+})
+
+const userModel = mongoose.model('user', userSchema)
+
+module.exports = {userModel}
