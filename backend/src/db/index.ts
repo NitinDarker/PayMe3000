e9a32187de94072a2e66c43401baba9388b37e7f
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-10-29 23:22:26 +0530

    feat: user account schema

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,15 +1,14 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv"
-dotenv.config()
+import mongoose, { Schema, model } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const mongo_url = process.env.MONGODB_URI as string;
-
 
 mongoose
   .connect(mongo_url)
   .then(() => console.log("MongoDB connected"))
   .catch((err: Error) => console.log("MongoDB Connection error: ", err));
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
   username: {
     type: String,
     required: true,
@@ -45,6 +44,19 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const userModel = mongoose.model("user", userSchema);
+const accountSchema = new Schema({
+  balance: {
+    type: Number,
+    require: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    required: true
+  },
+});
 
-export { userModel };
+const userModel = model("user", userSchema);
+const accountModel = model("account", accountSchema);
+
+export { userModel, accountModel };
